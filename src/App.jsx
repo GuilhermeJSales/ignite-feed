ee8b1cc499527ad3333c3f4bf67602b06af6ce39
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2023-05-01 22:56:30 -0300

    adicionada "automação de datas com fns" e atualização do componente post

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,55 @@ import { Sidebar } from "./components/Sidebar";
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id:1,
+    author: {
+      avatarUrl:'https://github.com/GuilhermeJSales.png',
+      name: 'Guilherme Jesus Sales',
+      role: 'Web Developer'
+    },
+    content:[
+      {type:'paragraph', content:'Fala galeraa 👋'},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é Impulse 🚀'},
+      {type:'link', content:'github.com/GuilhermeJSales'},
+    ],
+    publishedAt: new Date('2023-05-01 22:13:00')
+  },
+  {
+    id:2,
+    author: {
+      avatarUrl:'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content:[
+      {type:'paragraph', content:'Fala galeraa 👋'},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é Impulse 🚀'},
+      {type:'link', content:'github.com/diego3g'},
+    ],
+    publishedAt: new Date('2023-04-20 20:13:00')
+  }
+]
+
 export function App() {
   return (
     <div>    
-    <Header />
-
+      <Header />
     <div className={styles.wrapper}>
-
       <Sidebar />
-
       <main>
-          <Post
-            author="Guilherme Jesus Sales"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, illo. Consectetur doloribus facilis dolore repellendus, quis, alias dignissimos ipsa voluptates rem, quos quam explicabo fugit voluptatum! Tempore aperiam officia ducimus."
-          />
-
-          <Post
-            author="Rosana Jesus Sales"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, illo. Consectetur doloribus facilis dolore repellendus, quis, alias dignissimos ipsa voluptates rem, quos quam explicabo fugit voluptatum! Tempore aperiam officia ducimus."
-          />
-        </main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+      </main>
     </div>
    </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,47 @@
+import {format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
-export function Post(){
+
+
+export function Post({author, publishedAt, content}){
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {
+    locale:ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <dir className={styles.author}>
-            <Avatar src="https://github.com/GuilhermeJSales.png" />
+            <Avatar src={author.avatarUrl} />
             <div className={styles.authorInfo}>
-              <strong>Guilherme Jesus Sales</strong>
-              <span>Web Developer</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
         </dir>
 
-        <time title="29 de Abril de 2023 às 21:40h" dateTime='2023-04-29 21:40:30'>Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é Impulse 🚀</p>
-        <p><a href="https://github.com/GuilhermeJSales/nlw-return-impulse-web">Github.com/GuilhermeJSales</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if(line.type === 'paragraph'){
+            return <p>{line.content}</p>;
+          } else if(line.type === 'link'){
+            return <p><a href="#">{line.content}</a></p>;
+          }
+        })}
+
       </div>
 
       <form className={styles.commentForm}>

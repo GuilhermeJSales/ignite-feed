ce175aff573dbcaa765e1dde4f29b0a28df9a264
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2023-05-02 23:10:08 -0300

    adicionada funcionalidade para novos comentários e correção das keys nas lists

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,6 +6,7 @@ import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
+import { useState } from 'react'
 
 
 
@@ -18,6 +19,24 @@ export function Post({author, publishedAt, content}){
     locale: ptBR,
     addSuffix: true,
   })
+
+  const [comment, setComment] = useState([
+    'Post muito bacana, hein?',
+    
+  ])
+
+  const [newCommentText, setNewCommentText] = useState('');
+
+
+  function handleCreateNewComment(){
+      event.preventDefault()
+      setComment([...comment, newCommentText]);
+      setNewCommentText('');
+  }
+
+  function handleNewCommentChange(){
+    setNewCommentText(event.target.value)
+  }
 
   return (
     <article className={styles.post}>
@@ -36,18 +55,20 @@ export function Post({author, publishedAt, content}){
       <div className={styles.content}>
         {content.map(line => {
           if(line.type === 'paragraph'){
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if(line.type === 'link'){
-            return <p><a href="#">{line.content}</a></p>;
+            return <p key={line.content}><a href="#">{line.content}</a></p>;
           }
         })}
 
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea
+          onChange={handleNewCommentChange}
           placeholder='Escreva um comentário'
+          value={newCommentText}
         /> 
 
         <footer>
@@ -56,9 +77,9 @@ export function Post({author, publishedAt, content}){
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comment.map(comment => {
+          return <Comment key={comment} content={comment}/>
+        })}
       </div>
 
     </article>
